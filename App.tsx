80d69813983c4a1856f0e3cc0aa40def3a7cd500
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-05-07 09:39:32 +0600

    added styles

--- App.tsx
+++ App.tsx
@@ -11,4 +11,30 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
